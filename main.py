c43b8c4312317fa33cc284e64b2741b92217b387
Mateus Vicente <alemmateus98@gmail.com>
2022-08-02 12:42:42 -0400

    add the numeric buttons

--- main.py
+++ main.py
@@ -27,3 +27,33 @@ root = tk.Tk()
 root.geometry("300x275")
 textResult = tk.Text(root, height=2, width=16, font=("Arial", 24))
 textResult.grid(columnspan=5)
+
+button_1 = tk.Button(root, text="1", command=lambda: addToCalculation(1), width=5, font=("Arial", 14))
+button_1.grid(row=2, column=1)
+
+button_2 = tk.Button(root, text="2", command=lambda: addToCalculation(2), width=5, font=("Arial", 14))
+button_2.grid(row=2, column=2)
+
+button_3 = tk.Button(root, text="3", command=lambda: addToCalculation(3), width=5, font=("Arial", 14))
+button_3.grid(row=2, column=3)
+
+button_4 = tk.Button(root, text="4", command=lambda: addToCalculation(4), width=5, font=("Arial", 14))
+button_4.grid(row=3, column=1)
+
+button_5 = tk.Button(root, text="5", command=lambda: addToCalculation(5), width=5, font=("Arial", 14))
+button_5.grid(row=3, column=2)
+
+button_6 = tk.Button(root, text="6", command=lambda: addToCalculation(6), width=5, font=("Arial", 14))
+button_6.grid(row=3, column=3)
+
+button_7 = tk.Button(root, text="7", command=lambda: addToCalculation(7), width=5, font=("Arial", 14))
+button_7.grid(row=4, column=1)
+
+button_8 = tk.Button(root, text="8", command=lambda: addToCalculation(8), width=5, font=("Arial", 14))
+button_8.grid(row=4, column=2)
+
+button_9 = tk.Button(root, text="9", command=lambda: addToCalculation(9), width=5, font=("Arial", 14))
+button_9.grid(row=4, column=3)
+
+button_0 = tk.Button(root, text="0", command=lambda: addToCalculation(0), width=5, font=("Arial", 14))
+button_0.grid(row=5, column=2)
